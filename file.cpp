4ee8c8831bc5ed56daf9059a3d268c02a016d595
Ankit0003 <52325236+Ankit0003@users.noreply.github.com>
2021-06-03 14:32:00 +0530

    initial commit

---
+++ file.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	string l;string d;
+	ofstream fout;
+	fout.open("data");
+	cin>>l;
+	fout<<l<<endl;
+	fout.close();
+	ifstream fin;
+	fin.open("data");
+	fin>>d;
+	cout<<d;
+}
